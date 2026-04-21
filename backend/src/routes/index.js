bdefec2c1b9e20a43eb2d63bab3d0181adfe61da
dhanujaya738 <dhanujayanimsara@gmail.com>
2026-04-21 19:30:53 +0530

    Updated File With Report Routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import verificationRoutes from "./verification.routes.js";
+import reportRoutes from "./report.routes.js";
 
 const router = express.Router();
 
 // Routes will be registered here as development progresses.
 router.use("/verifications", verificationRoutes);
+router.use("/reports", reportRoutes);
 
 export default router;
