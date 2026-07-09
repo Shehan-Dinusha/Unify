2935466fa02c576decaa1cc304375d54e2874895
Shehan-Dinusha <136222997+Shehan-Dinusha@users.noreply.github.com>
2026-07-09 21:59:43 +0530

    Update sequelizeConfig.cjs

--- backend/src/config/sequelizeConfig.cjs
+++ backend/src/config/sequelizeConfig.cjs
@@ -1,7 +1,9 @@
 require("dotenv/config");
 
+const env = process.env.NODE_ENV || "development";
+
 module.exports = {
-  development: {
+  [env]: {
     username: process.env.DB_USER || "postgres",
     password: process.env.DB_PASSWORD || "postgres",
     database: process.env.DB_NAME || "Unify",
