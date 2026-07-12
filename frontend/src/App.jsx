19781050d8d744d794defb36fb48ab750c36006c
Shehan-Dinusha <136222997+Shehan-Dinusha@users.noreply.github.com>
2026-07-12 20:33:39 +0530

    Shehan learning feature finalizing (#155)
    * Trigger deploy workflow for PRs to dev
    
    * Pause toast timeout when hovered
    
    * Improve offline and auth navigation
    
    * fix a UI bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { ToastProvider } from "./components/common/Toast";
+import NetworkStatusBanner from "./components/common/NetworkStatusBanner";
 import { NotificationProvider } from "./context/NotificationContext";
 import { ChatProvider } from "./context/ChatContext";
 import { ClubOrderProvider } from "./context/ClubOrderContext";
@@ -9,6 +10,7 @@ import router from "./routes";
 function App() {
   return (
     <ToastProvider>
+      <NetworkStatusBanner />
       <NotificationProvider>
         <ClubOrderProvider>
           <ChatProvider>
